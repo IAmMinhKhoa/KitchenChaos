9dac3679a5d309f94fae7862df5396c492b5f6e2
Minh Khoa <minelady92@gmail.com>
2023-07-03 14:23:44 +0700

    1

---
+++ KitchenChaos/Assets/Scrips/PlateCompleteVisual.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSO_GameObjectList;
+
+
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+
+        foreach (KitchenObjectSO_GameObject kitchenObjetSOGameObject in kitchenObjectSO_GameObjectList)
+        {
+                kitchenObjetSOGameObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjetSOGameObject in kitchenObjectSO_GameObjectList)
+        {
+            if(kitchenObjetSOGameObject.kitchenObjectSO==e.kitchenObjectSO)
+            {
+                kitchenObjetSOGameObject.gameObject.SetActive(true);
+            }
+        }
+    }
+}
